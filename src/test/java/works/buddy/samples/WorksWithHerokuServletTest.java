e1a48c248f4101945ac0d88a9321746d95dd2ad5
phamdiep <dieppv0101@gmail.com>
2024-10-15 16:33:05 +0700

    [GFTCodeFix]: Update on  src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -24,7 +24,7 @@ public class WorksWithHerokuServletTest {
     private HttpServletResponse response;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
         servlet = new WorksWithHerokuServlet();
     }
@@ -36,7 +36,7 @@ public class WorksWithHerokuServletTest {
         when(response.getWriter()).thenReturn(writer);
 
         servlet.doGet(request, response);
-        assertEquals("Buddy Works with Heroku", new String( out.toByteArray(), "UTF-8"));
+        assertEquals("Buddy Works with Heroku", new String(out.toByteArray(), "UTF-8"));
     }
     // hello from Diep Pv for testing code
 }
