4e5e31224293b460b0b8ca60aa1f2066efdb2275
phamdiep <dieppv0101@gmail.com>
2024-10-21 14:23:24 +0700

    [GFTCodeFix]: Update on  src/main/java/works/buddy/samples/testjava.java

--- src/main/java/works/buddy/samples/testjava.java
+++ src/main/java/works/buddy/samples/testjava.java
@@ -1,18 +1,28 @@
+package com.example.filecreation;
+
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CreateFile {
-  public static void main(String[] args) {
-    try {
-      File myObj = new File("filename.txt");
-      if (myObj.createNewFile()) {
-        System.out.println("File created: " + myObj.getName());
-      } else {
-        System.out.println("File already exists.");
-      }
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+    private static final Logger logger = Logger.getLogger(CreateFile.class.getName());
+    private static final boolean DEBUG = false; // Make sure this debug feature is deactivated before delivering the code in production
+
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) {
+                if (DEBUG) {
+                    logger.log(Level.INFO, "File created: " + myObj.getName());
+                }
+            } else {
+                if (DEBUG) {
+                    logger.log(Level.INFO, "File already exists.");
+                }
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "An error occurred.", e);
+        }
     }
-  }
 }
